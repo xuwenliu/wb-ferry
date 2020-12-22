02d9afbfa2a97f4c153f117576558cecdd0b4fb4
xwl <xwl@xwldeMacBook-Pro.local>
2020-12-22 16:40:03 +0800

    feat:日志管理

--- audit/js/global.js
+++ audit/js/global.js
@@ -1,8 +1,31 @@
 $(function () {
-    $("#flod-btn").click(function () {
-        $('#sidebar').toggleClass('sidebar-flod');
-        $('#sidebar>ul').toggleClass('m-hide');
-        $('#m-content').toggleClass('content-flod')
-    });
-    $('[data-toggle="tooltipp"]').tooltip();
-})
+  $("#flod-btn").click(function () {
+    $("#sidebar").toggleClass("sidebar-flod");
+    $("#sidebar>ul").toggleClass("m-hide");
+    $("#m-content").toggleClass("content-flod");
+  });
+  $('[data-toggle="tooltipp"]').tooltip();
+
+  var Accordion = function (el, multiple) {
+    this.el = el || {};
+    this.multiple = multiple || false;
+
+    // Variables privadas
+    var links = this.el.find(".link");
+    // Evento
+    links.on("click", { el: this.el, multiple: this.multiple }, this.dropdown);
+  };
+
+  Accordion.prototype.dropdown = function (e) {
+    var $el = e.data.el;
+    ($this = $(this)), ($next = $this.next());
+
+    $next.slideToggle();
+
+    if (!e.data.multiple) {
+      $el.find(".submenu").not($next).slideUp().parent().removeClass("open");
+    }
+  };
+
+  var accordion = new Accordion($("#sidebar"), false);
+});
