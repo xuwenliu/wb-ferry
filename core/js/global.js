b3f44b75eb2fe1392c9edd0863b573242035d345
xwl <xwl@xwldeMacBook-Pro.local>
2020-12-26 19:56:42 +0800

    feat: 修改

--- core/js/global.js
+++ core/js/global.js
@@ -3,6 +3,7 @@ $(function () {
         $('#sidebar').toggleClass('sidebar-flod');
         $('#sidebar>ul').toggleClass('m-hide');
         $('#m-content').toggleClass('content-flod')
+        $(this).toggleClass('flod-btn-back');
     });
     $('[data-toggle="tooltipp"]').tooltip();
 })
