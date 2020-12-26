f6105232c6a225293ee6e26531ed6ede4a8e3310
xwl <xwl@xwldeMacBook-Pro.local>
2020-12-26 21:15:32 +0800

    feat: 修改2

--- audit/js/global.js
+++ audit/js/global.js
@@ -3,6 +3,8 @@ $(function () {
     $("#sidebar").toggleClass("sidebar-flod");
     $("#sidebar>ul").toggleClass("m-hide");
     $("#m-content").toggleClass("content-flod");
+    $(this).toggleClass('flod-btn-back');
+
   });
   $('[data-toggle="tooltipp"]').tooltip();
 
