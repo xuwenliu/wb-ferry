0cc267c1fc81cb82bf5cd8e60a5f0606a02c28f8
xwl <xwl@xwldeMacBook-Pro.local>
2020-12-31 13:25:28 +0800

    feat: 样式xiug

--- core/js/global.js
+++ core/js/global.js
@@ -1,9 +1,26 @@
 $(function () {
-    $("#flod-btn").click(function () {
-        $('#sidebar').toggleClass('sidebar-flod');
-        $('#sidebar>ul').toggleClass('m-hide');
-        $('#m-content').toggleClass('content-flod')
-        $(this).toggleClass('flod-btn-back');
-    });
-    $('[data-toggle="tooltipp"]').tooltip();
-})
+  $("#flod-btn").click(function () {
+    $("#sidebar").toggleClass("sidebar-flod");
+    $("#sidebar>ul").toggleClass("m-hide");
+    $("#m-content").toggleClass("content-flod");
+    $(this).toggleClass("flod-btn-back");
+  });
+  $('[data-toggle="tooltipp"]').tooltip();
+
+  // 通知消息删除
+  $(".view-close").click(function () {
+    var index = $(".view-close").index($(this));
+    var len = $(".notification .notification-body ul li").length;
+    if (len === 1) {
+      $(".notification").remove();
+    } else {
+      $(".notification .notification-body ul li").eq(index).remove();
+    }
+  });
+
+  $(".table-close").click(function () {
+    var index = $(".table-close").index($(this));
+    console.log(index);
+    $("#choosed_file tbody tr").eq(index).remove();
+  });
+});
