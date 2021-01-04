7531b7b2061b3e9eab9be770773bb399af20145a
xuwenliu <1436731293@qq.com>
2021-01-04 19:28:05 +0800

    feat: 150% 适配

--- core/js/global.js
+++ core/js/global.js
@@ -1,4 +1,13 @@
 $(function () {
+  var devicePixelRatio = {
+    '1': 1,
+    '1.25': 0.85,
+    '1.5': 0.7,
+    '1.75': 0.55,
+    '2': 1
+  };
+  document.body.style.zoom = devicePixelRatio[window.devicePixelRatio];
+
   $("#flod-btn").click(function () {
     $("#sidebar").toggleClass("sidebar-flod");
     $("#sidebar>ul").toggleClass("m-hide");
